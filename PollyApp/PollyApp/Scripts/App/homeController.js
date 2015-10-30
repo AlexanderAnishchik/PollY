138450e5ebda3355d36e744a07ef7246aed9c38f
AlexanderAnishchik <sanyaaxel94@gmail.com>
2015-10-31 00:55:03 +0200

    z

--- PollyApp/PollyApp/Scripts/App/homeController.js
+++ PollyApp/PollyApp/Scripts/App/homeController.js
@@ -1,23 +1,2 @@
-﻿angular
-    .module('myApp.ctrl.about', [])
-    .controller('aboutCtrl', ['$scope', '$window', function ($scope, $window) {
-
-        var w = angular.element($window);
-
-        $scope.version = "1.0.0";
-        $scope.windowWidth = 0;
-        $scope.windowHeight = 0;
-
-        var setDimensions = function () {
-            $scope.windowWidth = w.width();
-            $scope.windowHeight = w.height();
-        };
-
-        w.bind('resize', function () {
-            $scope.$apply(function () {
-                setDimensions();
-            });
-        });
-        setDimensions();
-
-    }]);
+﻿polly.controller('homeController', ['$scope', '$window', function ($scope, $window) {
+}]);
