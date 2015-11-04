f13d25caddd8e06e95aef3833779abf28f2f9c7e
AlexanderAnishchik <sanyaaxel94@gmail.com>
2015-11-04 23:52:27 +0200

    ф

---
+++ PollyApp/PollyApp/GenericRepository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+
+namespace PollyApp.GenericRepository
+{
+    public class Repository
+    {
+        public PollyEntities Context { get; set; }
+
+        public Repository()
+        {
+            Context = new PollyEntities();
+        }
+
+        public IEnumerable<TEntity> GetListRecords<TEntity>() where TEntity : class
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+        public void Add<TEntity>(TEntity ent) where TEntity : class
+        {
+            Context.Set<TEntity>().Add(ent);
+        }
+        public void Delete<TEntity>(int id) where TEntity : class
+        {
+            var el = Context.Set<TEntity>().Find(id);
+            Context.Set<TEntity>().Remove(el);
+        }
+        public void Update<TEntity>(TEntity ent) where TEntity : class
+        {
+            dynamic dynEntity = ent;
+            var temp = Context.Set<TEntity>().Find(dynEntity.Id);
+            Context.Entry<TEntity>(temp).CurrentValues.SetValues(ent);
+            Context.Entry<TEntity>(temp).State = EntityState.Modified;
+        }
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
