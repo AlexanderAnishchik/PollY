482ef4f3c8b8265b2148e6f82bbe7225bc7a5c35
Tema <asdasd@gmail.com>
2015-11-06 01:24:29 +0200

    qwe

--- PollyApp/PollyApp/Scripts/front-end/animate.js
+++ PollyApp/PollyApp/Scripts/front-end/animate.js
@@ -1,28 +1,33 @@
 ﻿
 $(document).ready(function () {
     var h = $(window).height();
-    
-   
+
+    $("#macbook .post").css("display","none");
     $("#header .post").eq(1).addClass('animated fadeInLeftBig');
     setTimeout(function () { $("#header .post").eq(0).addClass('animated fadeInDownBig'); }, 75);
     setTimeout(function () { $("#header .post").eq(2).addClass('animated fadeInRightBig'); }, 77);
     
 
     $(window).scroll(function () {
-        if (($(this).scrollTop() + h) >= $("#wrapper").offset().top) {
+        if (($(this).scrollTop() + h) >= $("#macbook").offset().top) {
             setTimeout(function () {
-                $("#wrapper .post").css({ visibility: "visible" });
-                $("#wrapper .post").eq(0).addClass('animated zoomIn');
-            }, 200);
+                
+                //$("#macbook .post").css({ visibility: "visible" });
+                $("#macbook .post").eq(0).addClass('animated fadeInLeft');
+            }, 0);
             
         }
-        if (($(this).scrollTop() + h) >= $("#about").offset().top) {
-            $("#about .post").css({ visibility: "visible" });
-            $("#about .post").eq(0).addClass('animated bounceInLeft');
-            $("#about .post").eq(1).addClass('animated bounceInRight');
-            $("#about .post").eq(2).addClass('animated bounceInLeft');
-            $("#about .post").eq(3).addClass('animated bounceInRight');
-            $("#about .post").eq(4).addClass('animated flipInX');
+        if (($(this).scrollTop() + h) >= $("#aboutfirst").offset().top) {
+            //$("#aboutfirst .post").css({ visibility: "visible" });
+            $("#aboutfirst .post").eq(0).addClass('animated fadeInUp');
+            $("#aboutfirst .post").eq(1).addClass('animated fadeInUp');
+           
+        }
+        if (($(this).scrollTop() + h) >= $("#aboutsecond").offset().top) {
+            $("#aboutsecond .post").css({ visibility: "visible" });
+            $("#aboutsecond .post").eq(0).addClass('animated fadeInUp');
+            $("#aboutsecond .post").eq(1).addClass('animated fadeInUp');
+
         }
         if (($(this).scrollTop() + h) >= $("#feedback").offset().top) {
             $("#feedback .post").css({ visibility: "visible" });
