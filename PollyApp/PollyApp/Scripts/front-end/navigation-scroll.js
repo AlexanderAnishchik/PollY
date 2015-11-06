4e69a9a8e318020433a586003755b4f9982a51fe
mozharovartem <mozharovartem@gmail.com>
2015-11-06 04:02:48 +0200

    qwe

---
+++ PollyApp/PollyApp/Scripts/front-end/navigation-scroll.js
@@ -0,0 +1,25 @@
+﻿
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop != 0) {
+            $("#navigation").animate({ backgroundColor: 'black' }, 400);
+        }
+        else {
+            $("#navigation").animate({ backgroundColor: 'transparent' }, 350);
+        }
+    });
+
+    $('#navigation').hover(
+    function (e) {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop != 0) {
+            $('#navigation').stop().animate({ backgroundColor: 'black' }, 400);
+        }
+    },
+    function (e) {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop != 0) {
+            $('#navigation').stop().animate({ backgroundColor: 'black' }, 400);
+        }
+    }
+    );
