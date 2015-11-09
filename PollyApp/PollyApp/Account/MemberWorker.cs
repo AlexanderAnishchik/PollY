2e87cd94e938ec13d4387d7d56e6238d5758f97f
AlexanderAnishchik <sanyaaxel94@gmail.com>
2015-11-09 10:40:52 +0200

    q

---
+++ PollyApp/PollyApp/Account/MemberWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace PollyApp.Account
+{
+    public static class MemberWorker
+    {
+        private const int SaltValueSize=4;
+        private static string[] HashingAlgorithms = new string[] { "SHA256", "MD5" };
+        public static Boolean Login(string email, string password)
+        {
+            return false;
+        }
+        private static String GetSalt()
+        {
+            return null;
+        }
+    }
+}
