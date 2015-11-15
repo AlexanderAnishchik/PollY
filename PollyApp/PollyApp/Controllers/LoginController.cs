f5de5b66553a420206a6bd671116763dc128ac27
AlexanderAnishchik <sanyaaxel94@gmail.com>
2015-11-15 22:52:31 +0200

    q

--- PollyApp/PollyApp/Controllers/LoginController.cs
+++ PollyApp/PollyApp/Controllers/LoginController.cs
@@ -8,9 +8,21 @@ namespace PollyApp.Account
 {
     public class SignInController
     {
-        public JsonResult SignIn(string login, string pass)
+        public JsonResult SignIn(String login, String pass)
         {
-            return null;
+            return new JsonResult() { Data = MemberWorker.Login(login, pass) };
+        }
+        public JsonResult SignUp(String email, String pass, String firstName, String lastName)
+        {
+            try
+            {
+                MemberWorker.Register(pass, email, firstName, lastName);
+                return new JsonResult() { Data = true };
+            }
+            catch (Exception)
+            {
+                return new JsonResult() { Data = false };
+            }
         }
     }
 }
