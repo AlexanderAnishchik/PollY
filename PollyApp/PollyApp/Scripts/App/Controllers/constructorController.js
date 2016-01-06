1669a0d8e2e11b2e7ac5dbe07a47e337dd3277bd
SanyaAxel <sanyaaxel94@gmail.com>
2016-01-06 18:28:16 +0200

    ZXC

--- PollyApp/PollyApp/Scripts/App/Controllers/constructorController.js
+++ PollyApp/PollyApp/Scripts/App/Controllers/constructorController.js
@@ -1,5 +1,6 @@
 ﻿PollyApp.controller('constructorController', ['$scope', 'headerKeeperService', function ($scope, headerKeeperService) {
     var me = this;
+    $scope.template = '';
     me.init = function () {
 
     };
