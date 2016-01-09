98fe5beace8ef100e2740da53d787dd8294c73db
SanyaAxel <sanyaaxel94@gmail.com>
2016-01-09 16:58:13 +0200

    м

---
+++ PollyApp/PollyApp/Helpers/DbEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PollyApp.Helpers
+{
+    public  class DbEnum
+    {       
+        public  readonly enum PollType
+        {
+            Single=1,
+            Multiply=2
+        }
+
+        public  readonly enum PollShare
+        {
+            FreeLink = 1,
+            PasswordLink =2,
+            CommunityLink=3
+        }
+        public  readonly enum PollAccess
+        {
+            Public = 1,
+            Anonymous = 2
+        }
+    }
+}
