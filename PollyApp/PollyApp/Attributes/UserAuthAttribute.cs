a022f268a2d1ce9384d3e35d34a714d5f178e5e7
AlexanderAnishchik <sanyaaxel94@gmail.com>
2016-02-06 15:18:52 +0200

    v

---
+++ PollyApp/PollyApp/Attributes/UserAuthAttribute.cs
@@ -0,0 +1,41 @@
+﻿using PollyApp.EFModel;
+using PollyApp.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+
+namespace PollyApp.Attributes
+{
+    public class UserAuthAttribute : System.Web.Mvc.ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session != null)
+            {
+                var user = filterContext.HttpContext.Session["user"] as User;
+                if (user != null)
+                    base.OnActionExecuting(filterContext);
+                else
+                {
+                    if (filterContext.HttpContext.User.Identity.IsAuthenticated == true)
+                    {
+                        User currUser = new User();
+                        using (var Db = new Repository())
+                        {
+                            currUser = Db.Context.Users.Where(x => x.Email == filterContext.HttpContext.User.Identity.Name).FirstOrDefault();
+                        }
+                        if (currUser != null)
+                            filterContext.HttpContext.Session["user"] = currUser;
+                        else
+                            throw new Exception("Error cookie");
+                    }
+                    else
+                        throw new Exception("Error cookie");
+                }
+            }
+        }
+    }
+}
--- PollyApp/PollyApp/Controllers/ConstructorController.cs
+++ PollyApp/PollyApp/Controllers/ConstructorController.cs
@@ -1,4 +1,5 @@
-﻿using PollyApp.EFModel;
+﻿using PollyApp.Attributes;
+using PollyApp.EFModel;
 using PollyApp.Helpers;
 using PollyApp.Models;
 using System;
@@ -18,25 +19,26 @@ namespace PollyApp.Controllers
         {
             return View();
         }
-    //        var Question = { value:"xcvxcv" }
-    //        var Answer = { value:"xcvxcv" }
-    //       var Answers =[Answer, Answer, Answer]
-    //     var PollUnits =[{Question, Answers},{Question,Answers
-    //},{Question,Answers}]
-        
-    //       jQuery.ajax({
-    //         method: "POST",
-    //         url: "/Constructor/SavePoll",
+        //        var Question = { value:"xcvxcv" }
+        //        var Answer = { value:"xcvxcv" }
+        //       var Answers =[Answer, Answer, Answer]
+        //     var PollUnits =[{Question, Answers},{Question,Answers
+        //},{Question,Answers}]
 
-    //                  data: JSON.stringify({PollUnits,PollName:"zxcxc"}),
-    //                  contentType: 'application/json; charset=utf-8',
+        //       jQuery.ajax({
+        //         method: "POST",
+        //         url: "/Constructor/SavePoll",
 
-    //        });
+        //                  data: JSON.stringify({PollUnits,PollName:"zxcxc"}),
+        //                  contentType: 'application/json; charset=utf-8',
+
+        //        });
+        [UserAuth]
         public string SavePoll(PollWrapper newPoll)
         {
             try
             {
-                newPoll.UserId=((dynamic)Session["user"]).Id;
+                newPoll.UserId = ((dynamic)Session["user"]).Id;
                 PollHelper.Save(newPoll);
                 return "S";
             }
