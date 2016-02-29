c5172d40e4a49fc6d2b78cdb98dd807f02a93526
mozharovartem <mozharovartem@gmail.com>
2016-02-29 23:06:05 +0200

    вапро

---
+++ PollyApp/PollyApp/Scripts/App/Controllers/dialogController.js
@@ -0,0 +1,19 @@
+﻿
+PollyApp.controller('dialogController', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+
+    $scope.showConfirm = function (event) {
+        var confirm = $mdDialog.confirm()
+           .title('Error')
+           .textContent('Question must be not null!')
+           .ariaLabel('YourPolly.com')
+           .targetEvent(event)
+           .ok('Yes')
+           .cancel('Cancel');
+        $mdDialog.show(confirm).then(function () {
+
+        }, function () {
+
+        });
+    };
+    
+}]);
