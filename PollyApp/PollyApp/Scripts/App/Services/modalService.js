f589e1adaf72d91ee7214c0a9e6612eac0e1d55d
mozharovartem <mozharovartem@gmail.com>
2016-03-01 01:55:11 +0200

    йцуцйуцйу

---
+++ PollyApp/PollyApp/Scripts/App/Services/modalService.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    PollyApp.service("modalService", [ "$mdDialog", function ($mdDialog) {
+        var self = this;
+        self.data = {};
+
+        self.showConfirm = function (object, success, cancel) {
+            var confirm = $mdDialog.confirm()
+               .title(object.title)
+               .textContent(object.textContent)
+               .ariaLabel(object.ariaLabel)
+               .targetEvent(object.event)
+               .ok('Remove')
+               .cancel('Cancel');
+            $mdDialog.show(confirm).then(success,
+                cancel);
+        };
+        self.showAlert = function (object) {
+            var alert = $mdDialog.alert()
+               .title(object.title)
+               .textContent(object.textContent)
+               .ariaLabel(object.ariaLabel)
+               .targetEvent(object.event)
+               .ok('Ok')
+            $mdDialog.show(alert).then(function () {
+
+            }, function () {
+
+            });
+        };
+    }]);
+})();
