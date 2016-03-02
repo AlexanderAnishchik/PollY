5c213761a507808bfe3deccc8f3b14c644e1cd75
mozharovartem <mozharovartem@gmail.com>
2016-03-02 22:57:00 +0200

    йцуцйуйцу

--- PollyApp/PollyApp/Scripts/App/Services/modalService.js
+++ PollyApp/PollyApp/Scripts/App/Services/modalService.js
@@ -14,18 +14,15 @@
             $mdDialog.show(confirm).then(success,
                 cancel);
         };
-        self.showAlert = function (object) {
+        self.showAlert = function (object,success,cancel) {
             var alert = $mdDialog.alert()
                .title(object.title)
                .textContent(object.textContent)
                .ariaLabel(object.ariaLabel)
                .targetEvent(object.event)
                .ok('Ok')
-            $mdDialog.show(alert).then(function () {
-
-            }, function () {
-
-            });
+            $mdDialog.show(alert).then(success,
+               cancel);
         };
         self.showCustomDialog = function (object, success, cancel) {
             var alert = $mdDialog.show({
@@ -34,6 +31,7 @@
                 parent: angular.element(document.body),
                 targetEvent: object.event,
                 clickOutsideToClose: object.outerClose,
+                escapeToClose : false
             }).then(success, cancel);
         }
     }]);
