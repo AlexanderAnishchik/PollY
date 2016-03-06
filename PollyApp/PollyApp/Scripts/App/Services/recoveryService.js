0a8bd2771517c016baf044f2d22a2294773184ef
AlexanderAnishchik <sanyaaxel94@gmail.com>
2016-03-06 03:06:16 +0200

    dfgdf

---
+++ PollyApp/PollyApp/Scripts/App/Services/recoveryService.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    PollyApp.service("recoveryService", [ function () {
+        var self = this;
+        self.setRecoveryPollData = function (data) {
+            if (localStorage != null) {
+                localStorage.setItem('poll', JSON.stringify(data));
+            }
+        };
+        self.getRecoveryPollData = function () {
+            var data = localStorage.getItem('poll');
+            if (data == null)
+                return null;
+            return JSON.parse(data);
+        };
+        self.clearRecoveryPollData = function () {
+            return localStorage.removeItem('poll');
+        };
+    }]);
+})();
