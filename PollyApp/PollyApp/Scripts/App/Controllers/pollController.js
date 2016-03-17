d8fba09ec1682779a5131f9ed175606ce83e72a0
AlexanderAnishchik <sanyaaxel94@gmail.com>
2016-03-18 00:31:46 +0200

    dfg

--- PollyApp/PollyApp/Scripts/App/Controllers/pollController.js
+++ PollyApp/PollyApp/Scripts/App/Controllers/pollController.js
@@ -1,6 +1,7 @@
 ﻿PollyApp.controller('pollController', ['$scope', 'pollSettingsFactory', '$http', function ($scope, pollSettingsFactory, $http) {
     var me = this;
     $scope.data = null;
+    me.result = [];
     me.init = function () {
         var pathArray = window.location.pathname.split('/');
         $http.post("/Constructor/GetPoll", { poll: pathArray[pathArray.length - 1] }).then(function (response) {
@@ -10,8 +11,25 @@
 
         });
     };
+    me.save = function () {
+        debugger;
+        var sendData = me.prepareData();
+    };
+    me.prepareData = function () {
+        var finishData = [];
+        for (var qw in me.result) {
+            var quest = { Id: qw };
+            for (var an in me.result[qw]) {
+                quest.Answers = [];
+                quest.Answers.push(an);
+            }
+            finishData.push(quest);
+        }
+        return finishData;
+    };
     me.parseAnswer = function (data) {
-        data.Questions.forEach((el, i)=> {
+        data.Questions.forEach((el, i) => {
+            me.result[el.Question.Id] = {};
             el.Answers.forEach((an, z) => {
                 data.Questions[i].Answers[z].Value = JSON.parse(an.Value).value;
             });
