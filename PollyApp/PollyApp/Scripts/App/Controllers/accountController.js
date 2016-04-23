1ca103b92f1e759782a3291d8a45594e4c14829f
AlexanderAnishchik <sanyaaxel94@gmail.com>
2016-04-23 22:32:26 +0300

    sad

--- PollyApp/PollyApp/Scripts/App/Controllers/accountController.js
+++ PollyApp/PollyApp/Scripts/App/Controllers/accountController.js
@@ -1,12 +1,27 @@
-﻿PollyApp.controller('accountController', ['$scope','$http', 'headerKeeperService', function ($scope,$http, headerKeeperService) {
+﻿PollyApp.controller('accountController', ['$scope', '$http', 'headerKeeperService', function ($scope, $http, headerKeeperService) {
     var me = this;
+    me.activePollCount = null;
+    me.activePollPercent = null;
+    me.votersCount = null;
     me.init = function () {
         me.getPollinformation();
     };
     me.getPollinformation = function () {
-        $http.get("/Account/GetUserPollInformation").then(function (response) {
+        $http.get("/Account/GetUserPollInformation")
+            .then(function (response) {
+                debugger;
+                if (response.data)
+                    me.setPollInf(response.data.userProject, response.data.votedProject, response.data.answerProjects);
+            }, function (response) {
 
-        })
+            });
+        me.setPollInf = function (userProject, votedProject, answerProjects) {
+            var allPoll = userProject.length;
+            me.activePollCount = userProject.filter(function (x) { return x.IsActive == true }).length;
+            me.activePollPercent = Math.ceil(me.activePollCount / allPoll * 100);
+            me.votersCount = votedProject;
+            me.answerProjects = answerProjects;
+        }
     }
     me.currentTab = "/Content/partial/account_page/home/home.html"
 }]);
