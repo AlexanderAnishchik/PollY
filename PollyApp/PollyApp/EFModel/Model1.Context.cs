a2f2fe5efbaf6f9518eaa6b8ebeb246b754dfb55
AlexanderAnishchik <sanyaaxel94@gmail.com>
2016-05-09 15:47:47 +0300

    qwe

--- PollyApp/PollyApp/EFModel/Model1.Context.cs
+++ PollyApp/PollyApp/EFModel/Model1.Context.cs
@@ -18,7 +18,7 @@ namespace PollyApp.EFModel
         public Entities()
             : base("name=Entities")
         {
-            this.Configuration.LazyLoadingEnabled = false;
+
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
