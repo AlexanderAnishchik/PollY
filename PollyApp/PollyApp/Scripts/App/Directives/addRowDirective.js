e777353f93f8ccc87546b51610cb7922ae8a2f16
mozharovartem <mozharovartem@gmail.com>
2016-05-17 12:34:54 +0300

    фывпрофылв

---
+++ PollyApp/PollyApp/Scripts/App/Directives/addRowDirective.js
@@ -0,0 +1,16 @@
+﻿PollyApp.directive('addrow', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $(document).ready(function () {
+                var elem = $(element);
+                elem.on('click', function () {
+                    console.log(elem.next());
+                    elem.next().slideToggle(200);
+
+                });
+              
+            });
+        }
+    }
+});
