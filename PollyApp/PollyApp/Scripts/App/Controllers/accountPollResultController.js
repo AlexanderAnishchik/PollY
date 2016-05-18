837003195210239236950bd9b47aca8a3f82f3d4
mozharovartem <mozharovartem@gmail.com>
2016-05-18 11:52:38 +0300

    фыв

---
+++ PollyApp/PollyApp/Scripts/App/Controllers/accountPollResultController.js
@@ -0,0 +1,7 @@
+﻿PollyApp.controller('accountPollResultController', ['$scope', '$http', 'headerKeeperService', function ($scope, $http, headerKeeperService) {
+    var me = this;
+    me.queList = [];
+    me.init = function () {
+       
+    };
+}]);
