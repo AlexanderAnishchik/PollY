9b9b524d7f26c6e24d54d42b65d080ef8d41c840
mozharovartem <mozharovartem@gmail.com>
2016-05-19 10:01:58 +0300

    am pm time

--- PollyApp/PollyApp/Scripts/App/Controllers/accountPollResultController.js
+++ PollyApp/PollyApp/Scripts/App/Controllers/accountPollResultController.js
@@ -12,7 +12,15 @@
             }
         }
         var date = new Date(parseInt(newstring));
-        date = date.getDate() + " / " + "0" + (parseInt(date.getMonth()) + 1) + " / " + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        date = date.getDate() + " / " + "0" + (parseInt(date.getMonth()) + 1) + " / " + date.getFullYear() + " " + hours + ":" + minutes + " " + ampm;
+        
         return date;
     }
     me.updateQuestions = function(data) {
