96166e4c16dcd5cf4e1a4013e421cd6ea10e7d30
vladislav.neychev <vladislav.neychev@chisw.com>
2016-08-04 17:32:32 +0300

    TASK 146

--- PollyApp/PollyApp/Scripts/App/AccountPolly.js
+++ PollyApp/PollyApp/Scripts/App/AccountPolly.js
@@ -1,31 +1,6 @@
 ﻿var AccountPolly = angular.module('AccountPolly', ['Polly','ngRoute', 'ui.router', 'ngAnimate', 'ui.bootstrap', 'ngMaterial', 'ngAnimation', "highcharts-ng"]);
 
 AccountPolly.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-	//'$routeProvider', '$locationProvider'
-    	
-    	//$locationProvider.html5Mode({
-    	//	enabled: true,
-    	//	requireBase: true
-    	//});
-
-    	//$routeProvider
-		//	.when('/home', {
-		//	    	templateUrl: '/Content/partial/account_page/home/home.html',
-		//	    	controller: 'accountController',
-		//	    	controllerAs: 'mainCtrl',
-		//	    	reloadOnSearch: false
-		//	})
-		//	.when('/polls', {
-		//		templateUrl: '/Content/partial/account_page/polls/polls.html',
-		//		controller: 'accountController',
-		//		controllerAs: 'mainCtrl',
-		//		reloadOnSearch: false
-		//	})
-		//	.otherwise({
-		//		redirectTo: '/home'
-		//	});
-
-	/*($stateProvider, $urlRouterProvider)*/
     	$stateProvider
             .state('home', {
             	url: '/home',
@@ -37,6 +12,13 @@ AccountPolly.config(['$stateProvider', '$urlRouterProvider', function ($statePro
 			.state('polls', {
 				url: '/polls',
 				templateUrl: '/Content/partial/account_page/polls/polls.html',
+				controller: 'accountController',
+				controllerAs: 'mainCtrl',
+				reloadOnSearch: false
+			})
+			.state('poll', {
+				url: '/poll/:pollId',
+				templateUrl: '/Content/partial/account_page/polls/poll.html',
 				controller: 'accountController',
 				controllerAs: 'mainCtrl',
 				reloadOnSearch: false
