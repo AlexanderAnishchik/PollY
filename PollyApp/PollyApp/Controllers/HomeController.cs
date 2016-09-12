19ea6e8e3d7c90b0fc24d611d6dcec8f5cb326b3
mozharovartem <mozharovartem@gmail.com>
2016-09-12 17:47:15 +0300

    sitemap.xml not worked

--- PollyApp/PollyApp/Controllers/HomeController.cs
+++ PollyApp/PollyApp/Controllers/HomeController.cs
@@ -9,15 +9,18 @@ using System.Linq;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using System.Data.Entity;
 using System.Web.Security;
 using PollyApp.Models;
 using System.Threading.Tasks;
 using SendGrid;
 using System.Net.Mail;
+using PollyApp.Seo;
 
 namespace PollyApp.Controllers
 {
+
     public class HomeController : Controller
     {
         private GenericRepository.Repository Db = new GenericRepository.Repository();
@@ -31,9 +34,62 @@ namespace PollyApp.Controllers
             PollHelper.ChartPollData("0057436918",users);
             return View();
         }
+
+        public IReadOnlyCollection<SitemapNode> GetSitemapNodes(UrlHelper urlHelper)
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>();
+
+            nodes.Add(
+                new SitemapNode()
+                {
+                    Url = urlHelper.AbsoluteRouteUrl("HomeGetIndex"),
+                    Priority = 1
+                });
+            nodes.Add(
+                new SitemapNode()
+                {
+                    Url = urlHelper.AbsoluteRouteUrl("ConstructorGetIndex"),
+                    Priority = 0.9
+                });
+
+            return nodes;
+        }
+        public string GetSitemapDocument(IEnumerable<SitemapNode> sitemapNodes)
+        {
+            XNamespace xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            XElement root = new XElement(xmlns + "urlset");
+
+            foreach (SitemapNode sitemapNode in sitemapNodes)
+            {
+                XElement urlElement = new XElement(
+                      xmlns + "url",
+                      new XElement(xmlns + "loc", Uri.EscapeUriString(sitemapNode.Url)),
+                      sitemapNode.LastModified == null ? null : new XElement(
+                          xmlns + "lastmod",
+                          sitemapNode.LastModified.Value.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:sszzz")),
+                      sitemapNode.Frequency == null ? null : new XElement(
+                          xmlns + "changefreq",
+                          sitemapNode.Frequency.Value.ToString().ToLowerInvariant()),
+                      sitemapNode.Priority == null ? null : new XElement(
+                          xmlns + "priority",
+                          sitemapNode.Priority.Value.ToString()));
+                      root.Add(urlElement);
+              }
+
+              XDocument document = new XDocument(root);
+              return document.ToString();
+        }
+
+        [Route("sitemap.xml")]
+        public ActionResult SitemapXml()
+        {
+            var sitemapNodes = GetSitemapNodes(this.Url);
+            string xml = GetSitemapDocument(sitemapNodes);
+            Response.ContentType = "text/xml";
+            return this.Content(xml);
+        }
+
         [HttpPost]
-
-
         public async Task<ActionResult> SendEmail(EmailModel mail)
         {
             SendGridMessage myMessage = new SendGridMessage();
